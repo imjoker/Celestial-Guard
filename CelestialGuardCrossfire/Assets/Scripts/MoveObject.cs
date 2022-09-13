390be01944091579c0051894fea94535b3b281e2
d-dcarl <47509902+d-dcarl@users.noreply.github.com>
2022-09-13 03:00:20 -0600

    Level design is passable. plz playtest

---
+++ CelestialGuardCrossfire/Assets/Scripts/MoveObject.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour
+{
+    [SerializeField] Transform[] Positions;
+    [SerializeField] float ObjectSpeed;
+
+
+    int NextPosIndex;
+    Transform NextPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextPos = Positions[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveGameObject();
+    }
+
+    void MoveGameObject()
+    {
+        if(transform.position == NextPos.position)
+        {
+            NextPosIndex++;
+            if (NextPosIndex >= Positions.Length)
+            {
+                NextPosIndex = 0;
+            }
+            NextPos = Positions[NextPosIndex];
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, NextPos.position, ObjectSpeed * Time.deltaTime);
+        }
+    }
+}
